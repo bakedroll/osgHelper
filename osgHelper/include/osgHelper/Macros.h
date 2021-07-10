e8558b481d6fa3ed1471c40d25eced4879e1d482
Daniel Borchart <daniel.borchart@protonmail.com>
2021-07-10 17:39:50 +0200

    added missing include

--- osgHelper/include/osgHelper/Macros.h
+++ osgHelper/include/osgHelper/Macros.h
@@ -1,4 +1,6 @@
 #pragma once
 
+#include <cassert>
+
 #define assert_return(cond, ...) if (!(cond)) { assert(false); return __VA_ARGS__; }
 #define assert_continue(cond) if (!(cond)) { assert(false); continue; }
